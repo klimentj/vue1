d83594ecf62489d02f3953ab977a502ce20bad04
Kliment.Jancheski <Kliment.Jancheski@seavus.com>
2019-09-24 00:30:34 +0200

    First commit

---
+++ src/Blogs.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="blogs">
+        <h2>{{blogTitle}}</h2>
+        <button @click="changeTitle">Change Title</button>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{post.title}}</h3>
+            <p>{{post.body | snippet}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Blogs',
+    data(){
+        return{
+            blogTitle: 'Blogs',
+            posts:[]
+        }
+    },
+    methods:{
+        changeTitle(){
+            this.blogTitle = "edited title"
+        }
+    },
+    beforeCreate(){
+        //alert('before create hook')
+    },
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(response=>{
+            this.posts = response.data
+        }).catch(err=>{
+            console.log(err)
+        })
+    },
+    beforeUpdate(){
+        //alert('beforeUpdate')
+    }
+}
+</script>
+
